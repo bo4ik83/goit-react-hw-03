69dea486fb84ac25bd6d3ab8834f39b13a3fba23
Denis Bochev <denisbo4ev@gmail.com>
2024-12-17 22:19:54 +0200

    add-localstorage

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import ContactList from "../ContactList/ContactList";
 import ContactForm from "../ContactForm/ContactForm.jsx";
 import SearchBox from "../SearchBox/SearchBox";
 import "./App.css";
 
+const LOCAL_STORAGE_KEY = "contacts";
+
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
+
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
