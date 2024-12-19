5441b763a2dd5d768a3f5dc79f8d3717bc548033
bo4ik83 <denisbo4ev@gmail.com>
2024-12-19 13:45:59 +0200

    contact-form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,17 @@
 import PropTypes from "prop-types";
 import styles from "./Contact.module.css";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 
 const Contact = ({ name, number, onDelete }) => (
   <div className={styles.contactCard}>
     <div className={styles.info}>
       <div className={styles.name}>
-        <span className={styles.icon}>
-          <img src="/svg/user.svg" alt="name" width={18} height={18} />
-        </span>{" "}
+        <span className={styles.icon}><FaUser/></span>{" "}
         {name}
       </div>
       <div className={styles.number}>
-        <span className={styles.icon}>
-          <img src="/svg/phone.svg" alt="number" width={18} height={18} />
-        </span>{" "}
+        <span className={styles.icon}><FaPhoneAlt/></span>{" "}
         {number}
       </div>
     </div>
